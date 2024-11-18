9585cbc8bf37d773f76d53bb976b93751e268fe2
Gustavo Gondim <gustavospgondim@gmail.com>
2024-11-18 16:41:36 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -14,13 +14,17 @@ curl -o ~/.ssh/$ID_RSA_GPG $BASE_URL/$ID_RSA_GPG
 curl -o ~/.ssh/$ID_RSA_PUB_GPG $BASE_URL/$ID_RSA_PUB_GPG
 
 echo "MASTER KEY:"
-read -s PASSPHRASE
+stty -echo
+read PASSPHRASE
+stty echo
 
-echo "$PASSPHRASE" | gpg --batch --yes --passphrase-fd 0 --decrypt ~/.ssh/id_rsa.gpg > ~/.ssh/id_rsa
+gpg --batch --yes --passphrase "$PASSPHRASE" --decrypt ~/.ssh/id_rsa.gpg > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 
-echo "$PASSPHRASE" | gpg --batch --yes --passphrase-fd 0 --decrypt ~/.ssh/id_rsa.pub.gpg > ~/.ssh/id_rsa.pub
+gpg --batch --yes --passphrase "$PASSPHRASE" --decrypt ~/.ssh/id_rsa.pub.gpg > ~/.ssh/id_rsa.pub
 chmod 644 ~/.ssh/id_rsa.pub
 
 chmod 600 ~/.ssh/id_rsa
 chmod 644 ~/.ssh/id_rsa.pub
+
+
